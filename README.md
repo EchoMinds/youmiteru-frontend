e18a1740c15114b2f16b73b0b7ad2269182b0066
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2024-01-10 16:35:35 +0100

    Init Project (#2)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # youmiteru-frontend
 Frontend for the youmiteru website.
+
+## To run this application you should do next steps:
+- Open a terminal
+- run following commands:
+  
+  cd frontend
+
+  npm install
+  
+  npm run dev
---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+        watch: {
+            usePolling: true
+        },
+        host: true, // Here
+        strictPort: true,
+        port: 5173
+  },
+  plugins: [react()],
+})
