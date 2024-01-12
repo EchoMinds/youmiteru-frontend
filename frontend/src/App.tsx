920c3d39a1209f2894d74b4243d9e90b40fe865b
Kostek <ksytnik73kwer@gmail.com>
2024-01-12 21:11:32 +0100

    Add Routers to our pages. Add some Routers to "App.tsx".

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
 import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ROUTERS } from "./constants/routes";
 
 function App() {
 
-  return (
-    <>
-      Start Working!
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* We will create our pages later it's just an example */}
+                <Route 
+                    path={ROUTERS.HOME} 
+                    element={<h1>Our main page</h1>} 
+                />
+
+                <Route 
+                    path="/about" 
+                    element={<h1>Our first pagination</h1>} 
+                />
+                
+                <Route 
+                    path="*" 
+                    element={<h1>!Undefined Page! please return on main page</h1>} 
+                /> 
+            </Routes>  
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTERS = {
+    HOME: "/",
+    LOGIN: "/login",
+    REGISTER: "/register",
+    LIBRARY: "/library",
+    HELP: "/help",
+    PROFILE: "/profile",
+    ANIME : "/anime",
+    SETTINGS: "/settings",
+};
