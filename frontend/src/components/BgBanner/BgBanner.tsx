150b8712b3f66934305e749221ba8ed4c9db31a8
Taps73r <povroznykmisha@gmail.com>
2024-01-20 22:52:23 +0200

    Add styles for Banner; Add useState and callback-function to change photo. Add WatchButton component

--- frontend/src/components/BgBanner/BgBanner.tsx
+++ frontend/src/components/BgBanner/BgBanner.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDashBoardQuery } from "../../hooks/useDashBoardQuery";
 import SliderBanner from "../SliderBanner/SliderBanner";
 import "./BgBanner.scss";
@@ -6,9 +7,27 @@ function BgBanner(): JSX.Element {
     const DashBoardQuery = useDashBoardQuery();
     const banners = DashBoardQuery.data?.banners;
 
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+    const handleSlideChange = (index: number) => {
+        setCurrentSlideIndex(index);
+    };
+
+    const currentBanner = banners?.[currentSlideIndex];
+
+    const bgBannerStyles = {
+        backgroundImage: `url(${currentBanner?.season_image_url})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+    };
+
+
     return (
-        <div className="bg-banner">
-            <SliderBanner bannerData={banners} />
+        <div className="bg-banner" style={bgBannerStyles}>
+            {currentBanner && (
+                <SliderBanner bannerData={banners} onSlideChange={handleSlideChange} />
+            )}
         </div>
     );
 }
