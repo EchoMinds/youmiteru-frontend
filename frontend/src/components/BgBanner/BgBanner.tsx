7e292e821a33dbadad27b2a0e33b698b3a759463
Taps73r <povroznykmisha@gmail.com>
2024-01-20 18:09:54 +0200

    Add useDashBoardQuery to BgBanner, delete props in BgBanner

--- frontend/src/components/BgBanner/BgBanner.tsx
+++ frontend/src/components/BgBanner/BgBanner.tsx
@@ -1,11 +1,15 @@
+import { useDashBoardQuery } from "../../hooks/useDashBoardQuery";
 import { Banner } from "../../types/Banner";
+import SliderBanner from "../SliderBanner/SliderBanner";
 import "./BgBanner.scss";
 
-function BgBanner({ bannerData }: Banner): JSX.Element {
+function BgBanner(): JSX.Element {
+    const DashBoardQuery = useDashBoardQuery();
+    const banners = DashBoardQuery.data?.banners;
 
     return (
         <div className="bg-banner">
-
+            <SliderBanner bannerData={banners} />
         </div>
     );
 }
