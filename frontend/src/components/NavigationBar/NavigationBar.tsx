a015f3976731866e7e9787c8f58fa785da9c2d9b
Kostek <ksytnik73kwer@gmail.com>
2024-01-21 18:55:58 +0100

    Update "App.scss". Update NavigationBar and styles for it.

--- frontend/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/NavigationBar/NavigationBar.tsx
@@ -1,57 +1,62 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { ROUTES } from "../../constants/routes";
 
 import "./NavigationBar.scss";
-import classNames from "classnames";
 import WebLogo from "@/assets/icons/website-logo.png";
 import SearchIcon from "@/assets/icons/search-icon.svg";
 import UserProfileIcon from "@/assets/icons/user_icon.svg";
 
 function NavigationBar() {
-    
     return (
         <nav className="navigation-bar">
-            <Link to={ROUTES.HOME}>
-                <div className="web-logo">
-                    <img src={WebLogo} alt="WebSite Logo" />
-                </div>
-                
-                <span className="center-panel">
-                    Главное
-                </span>
-            </Link>
-            
-            <Link to={ROUTES.CATALOG}>
-                <span className="center-panel">
-                    Каталог
-                </span>
-            </Link>
-
-            <Link to={ROUTES.HELP_PROJECT}>
-                <span className="center-panel">
-                    Поддержка проекта
-                </span>
-            </Link>
-
-            <Link to={ROUTES.HELP}>
-                <span className="center-panel">
-                    Обратная связь
-                </span>
-            </Link>
-
-            <div className="right-panel">
-                <img src={SearchIcon} alt="Search" />
-            </div>
-            
-            <div className={classNames("user-profile", "right-panel")}>
-                <img src={UserProfileIcon} />
+            <div className="navigation-bar__left-panel">
+                <Link to={ROUTES.HOME}>
+                    <div className="web-logo">
+                        <img src={WebLogo} alt="WebSite Logo" />
+                    </div>
+                </Link>
             </div>
 
-            <Link to={ROUTES.LOGIN}>
-                <div className={classNames("login", "right-panel")}>
-                    Войти
-                </div>
-            </Link>
+            <ul className="navigation-bar__center-panel">
+                <li>
+                    <Link to={ROUTES.HOME}>
+                        <div className="home">Главная</div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.CATALOG}>
+                        <div className="catalog">Каталог</div>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to={ROUTES.SPONSOR_PROJECT}>
+                        <div className="sponsor-project">Поддержка проекта</div>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to={ROUTES.HELP}>
+                        <div className="help">Обратная связь</div>
+                    </Link>
+                </li>
+            </ul>
+
+            <div className="navigation-bar__right-panel">
+                <button className="search">
+                    <img src={SearchIcon} alt="Search" />
+                </button>
+
+                <Link to={ROUTES.PROFILE}>
+                    <button className="user-profile">
+                        <img src={UserProfileIcon} className="user-icon" alt="User Profile" />
+                    </button>
+                </Link>
+
+                <Link to={ROUTES.LOGIN}>
+                    <div className="login">Войти</div>
+                </Link>
+            </div>
         </nav>
     );
 }
