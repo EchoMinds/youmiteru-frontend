b9a5f3ad4b4362fc22a3f3b766f6a6590e8c0ad8
Kostek <ksytnik73kwer@gmail.com>
2024-01-31 20:19:35 +0100

    Refactor styles for SectionSeasons and add SliderSeasons component. Fix "App.tsx".

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,6 @@
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ROUTES } from "./constants/routes";
-import SectionSeasons from "./components/SectionSeasons/SectionSeasons";
-
-function App() {
-  return (
-    <BrowserRouter>
-      {/* <Routes>
-        {/* We will create our pages later it's just an example }
-        <Route path={ROUTES.HOME} element={<h1>Our main page</h1>} />
-=======
 import { useDashBoardQuery } from "./hooks/useDashBoardQuery";
 
 
@@ -28,23 +19,28 @@ function App() {
     const popular_seasons = DashBoardQuery.data?.popular_seasons;
     const announced_seasons = DashBoardQuery.data?.announced_seasons;
     const released_seasons = DashBoardQuery.data?.released_seasons;
->>>>>>> 104fe937c21c792acbfaf2fb29c76e7c8a61f79d
 
-        <Route path="/about" element={<h1>Our first pagination</h1>} />
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* We will create our pages later it's just an example */}
+                <Route 
+                    path={ROUTES.HOME} 
+                    element={<h1>Our home page</h1>} 
+                />
 
-        <Route
-          path="*"
-          element={<h1>!Undefined Page! please come back at main page</h1>}
-        />
-      </Routes> */}
-      <h2>Онгоинги</h2>
-      <SectionSeasons />
-      <h2>Анонсы</h2>
-      <SectionSeasons />
-      <h2>С высокой оценкой</h2>
-      <SectionSeasons />
-    </BrowserRouter>
-  );
+                <Route 
+                    path={ROUTES.HELP}
+                    element={<h1>Help Page</h1>} 
+                />
+                
+                <Route 
+                    path="*" 
+                    element={<h1>!Undefined Page! please come back at main page</h1>} 
+                /> 
+            </Routes>  
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/SliderSeasons/SliderSeasons.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import Slider from "react-slick";
+import "./SliderSeasons.scss";
+import PrevArrow from "../SliderButtons/PrevArrow";
+import NextArrow from "../SliderButtons/NextArrow";
+import { Season } from "../../types/Season";
+
+interface SliderSeasonsProps {
+    sliderData: Season[] | [];
+    onSlideChange?: (index: number) => void;
+}
+
+interface ResponsiveSettings {
+    breakpoint: number;
+}
+
+interface SliderSeasonsSettings {
+    infinite: boolean;
+    speed: number;
+    slidesToShow: number;
+    slidesToScroll: number;
+    centerMode: boolean;
+    centerPadding: string;
+    beforeChange?: (current: number, next: number) => void;
+    nextArrow?: JSX.Element;
+    prevArrow?: JSX.Element;
+    responsive?: ResponsiveSettings[];
+    arrows: boolean;
+}
+
+function SliderSeasons({ sliderData, onSlideChange }: SliderSeasonsProps) {
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const settings: SliderSeasonsSettings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: "0",
+        beforeChange: (_, next) => {
+            setSlideIndex(next);
+            if (onSlideChange) {
+                onSlideChange(next);
+            }
+        },
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+        ]
+    };
+
+    return (
+        <div className="slider-season">
+            <Slider {...settings}>
+                {sliderData && 
+                    sliderData.map((item, index) => (
+                        <div key={index} className="slider-season__card">
+                            <div className="slider-season__card__top">
+                                <img
+                                    src={item.season_image_url}
+                                    alt={item.season_name}
+                                />
+                                <h3>{item.season_name}</h3>
+                            </div>
+
+                            <div
+                                className="slider-season__card__description">
+                                {item.season_description}
+                            </div>
+
+                        </div>
+                    ))
+                }
+            </Slider>
+        </div>
+    );
+}
+
+export default SliderSeasons;
