e34c28698e0dc48183284ade18cb6eb45fe9ca57
Misha Povroznyk <124689927+Taps73r@users.noreply.github.com>
2024-01-31 20:56:03 +0200

    Delete EmptyArrow, update responsive slider settings interface,

--- frontend/src/components/BgBanner/BgBanner.tsx
+++ frontend/src/components/BgBanner/BgBanner.tsx
@@ -1,50 +1,98 @@
-import "./BgBanner.scss";
+import "./SliderBanner.scss";
+import Slider from "react-slick";
 import { useState } from "react";
-import WatchButton from "../WatchButton/WatchButton";
-import SliderBanner from "../SliderBanner/SliderBanner";
+import NextArrow from "../SliderButtons/NextArrow";
+import PrevArrow from "../SliderButtons/PrevArrow";
 import { Banner } from "../../types/Banner";
 
-interface BgBannerProps {
-    bannerData: Banner[] | [];
+interface SliderBannerProps {
+    bannerData: Banner[];
+    onSlideChange?: (index: number) => void;
 }
 
-function BgBanner({ bannerData }: BgBannerProps): JSX.Element {
-    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+interface ResponsiveSliderSettings {
+    focusOnSelect: boolean;
+    autoplay: boolean;
+    autoplaySpeed: number;
+    arrows: boolean;
+}
 
-    const handleSlideChange = (index: number) => {
-        setCurrentSlideIndex(index);
+interface ResponsiveSettings {
+    breakpoint: number;
+    settings: ResponsiveSliderSettings;
+}
+
+interface SliderBannerSettings {
+    infinite: boolean;
+    speed: number;
+    slidesToShow: number;
+    slidesToScroll: number;
+    centerMode: boolean;
+    centerPadding: string;
+    beforeChange?: (current: number, next: number) => void;
+    nextArrow?: JSX.Element;
+    prevArrow?: JSX.Element;
+    responsive?: ResponsiveSettings[];
+}
+
+function SliderBanner({
+    bannerData,
+    onSlideChange,
+}: SliderBannerProps): JSX.Element {
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const ResponsiveSliderSettings: ResponsiveSliderSettings = {
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        arrows: false,
     };
 
-    const currentBanner = bannerData?.[currentSlideIndex];
-
-    const bgBannerStyles = {
-        backgroundImage: `url(${currentBanner?.season_image_url})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center"
+    const settings: SliderBannerSettings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: "0",
+        beforeChange: (_, next) => {
+            setSlideIndex(next);
+            if (onSlideChange) {
+                onSlideChange(next);
+            }
+        },
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: ResponsiveSliderSettings,
+            },
+        ],
     };
 
     return (
-        <>
-            {currentBanner && (
-                <div className="bg-banner" style={bgBannerStyles}>
-                    <div className="bg-banner__season-text">
-                        <h1>{currentBanner?.season_name}</h1>
-                        <p>{currentBanner?.season_description}</p>
-                    </div>
-                    <div className="bg-banner__watch-buttons">
-                        <WatchButton isWatchNow={true} />
-                        <WatchButton isWatchTrailer={true} />
-                    </div>
-                    <div className="bg-banner__slider">
-                        <SliderBanner
-                            bannerData={bannerData}
-                            onSlideChange={handleSlideChange}
-                        />
-                    </div>
-                </div>
-            )}
-        </>
+        <div className="slider-banner">
+            <Slider {...settings}>
+                {bannerData &&
+                    bannerData.map((img, index) => (
+                        <div
+                            className={
+                                index === slideIndex
+                                    ? "slider-banner__slide slider-banner__slide-active"
+                                    : "slider-banner__slide"
+                            }
+                            key={index}
+                        >
+                            <img
+                                src={img.season_image_url}
+                                alt="slider picture"
+                            />
+                        </div>
+                    ))}
+            </Slider>
+        </div>
     );
 }
 
-export default BgBanner;
+export default SliderBanner;
