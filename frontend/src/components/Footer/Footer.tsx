8c036f5a076e664e4f5a0c57eb92e5675bbac9c9
Taps73r <povroznykmisha@gmail.com>
2024-02-02 20:08:38 +0200

    Create tsx component

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+export function Footer(): JSX.Element {
+    return (
+        <footer className="footer">
+            <h1>
+                <a href="">Связаться со мной</a>
+            </h1>
+        </footer>
+    );
+}
