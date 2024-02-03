16b4cd45c95a80372b76f1c9554bf7a9f6f03d01
Misha Povroznyk <124689927+Taps73r@users.noreply.github.com>
2024-02-03 17:34:19 +0200

    Feature/component/footer (#30)
    * Create footer files
    
    * Create tsx component
    
    * Add styles
    
    * Add icons and change footer component
    
    * import icons
    
    * Delete default margin and padding
    
    * Add styles, change Links
    
    * Add position for element, hover for links
    
    * change website logo to youmitery text, delete hover for h1 tag, add transition 0.5s for links
    
    * change margin
    
    * add styles for youmiteru text, add responsive style and position
    
    * create misc.ts, add links to other websites there. Import these links to footer
    
    * add new routes for footer
    
    * Import new routes to footer

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -7,6 +7,12 @@ export const ROUTES = {
     SPONSOR_PROJECT: "/sponsor-project",
     HELP: "/help",
     PROFILE: "/profile",
-    ANIME : "/anime",
+    ANIME: "/anime",
     SETTINGS: "/settings",
+    FAQ: "/faq",
+    CONTACTS: "/contacts",
+    ADVERTISMENT: "/advertisment",
+    USER_AGREEMENT: "/user-agreement",
+    PRIVACY_POLICY: "/privacy-policy",
+    COMPLAINT: "/complaint",
 };
