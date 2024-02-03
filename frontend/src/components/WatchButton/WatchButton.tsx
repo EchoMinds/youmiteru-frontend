33efcdfd941845f53a05d5efe32f8df44f8ca72a
Kostek <ksytnik73kwer@gmail.com>
2024-02-03 21:53:31 +0100

    Update SliderSeasons component. Update params to WatchButton in BgBanner component. Update WatchButton component. Update Routes.

--- frontend/src/components/WatchButton/WatchButton.tsx
+++ frontend/src/components/WatchButton/WatchButton.tsx
@@ -1,29 +1,45 @@
 import "./WatchButton.scss";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../constants/routes";
 
 interface WatchButtonProps {
     isWatchNow?: boolean;
     isWatchTrailer?: boolean;
+    season_id: number;
 }
 
-function WatchButton({ isWatchNow = false, isWatchTrailer = false }: WatchButtonProps) {
-
+function WatchButton({
+    isWatchNow = false,
+    isWatchTrailer = false,
+    season_id,
+}: WatchButtonProps) {
     return (
         <>
             {isWatchNow && (
-                <div className="watch-button-now">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="21" viewBox="0 0 22 21" fill="none">
-                        <path d="M0 2.87313C0 0.658533 2.70462 -0.719851 4.85991 0.394617L19.4251 7.93915C21.5626 9.04501 21.5626 11.7874 19.4251 12.8947L4.85991 20.4364C2.70462 21.5523 0 20.1739 0 17.9608V2.87313Z" fill="white" />
+                <Link
+                    to={`${ROUTES.SEASON}/${season_id}`}
+                    className="watch-button-now">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="22"
+                        height="21"
+                        viewBox="0 0 22 21"
+                        fill="none">
+                        <path
+                            d="M0 2.87313C0 0.658533 2.70462 -0.719851 4.85991 0.394617L19.4251 7.93915C21.5626 9.04501 21.5626 11.7874 19.4251 12.8947L4.85991 20.4364C2.70462 21.5523 0 20.1739 0 17.9608V2.87313Z"
+                            fill="white"
+                        />
                     </svg>
-                    <div className="watch-button-now__text">
-                        {"Watch Now"}
-                    </div>
-                </div>
+                    <div className="watch-button-now__text">{"Watch Now"}</div>
+                </Link>
             )}
 
             {isWatchTrailer && (
-                <div className="watch-button-trailer">
+                <Link
+                    to={`${ROUTES.TRAILER}/${season_id}`}
+                    className="watch-button-trailer">
                     {"Watch Trailer"}
-                </div>
+                </Link>
             )}
         </>
     );
