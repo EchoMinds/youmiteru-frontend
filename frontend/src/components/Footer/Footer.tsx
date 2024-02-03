140a89b815f6357fb41215fce6893c210179104e
Taps73r <povroznykmisha@gmail.com>
2024-02-03 14:12:49 +0200

    Add icons and change footer component

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,11 +1,45 @@
+import { Link } from "react-router-dom";
 import "./Footer.scss";
 
 export function Footer(): JSX.Element {
     return (
         <footer className="footer">
-            <h1>
-                <a href="">Связаться со мной</a>
-            </h1>
+            <div className="footer__second-part">
+                <h1>О нас</h1>
+                <ul>
+                    <li>
+                        <Link to="">Ntrdfsa</Link>
+                    </li>
+                    <li>
+                        <Link to="">Ntrdfsa</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__second-part">
+                <h1>Помощь</h1>
+                <ul>
+                    <li>
+                        <Link to="">Ntrdfsa</Link>
+                    </li>
+                    <li>
+                        <Link to="">Ntrdfsa</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__second-part">
+                <h1>Другое</h1>
+                <ul>
+                    <li>
+                        <Link to="">Ntrdfsa</Link>
+                    </li>
+                    <li>
+                        <Link to="">Ntrdfsa</Link>
+                    </li>
+                    <li>
+                        <Link to="">Ntrdfsa</Link>
+                    </li>
+                </ul>
+            </div>
         </footer>
     );
 }
