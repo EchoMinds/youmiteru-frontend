57414bc529f48aa2b8b38758793bbf2a0ce88e8a
Misha Povroznyk <124689927+Taps73r@users.noreply.github.com>
2024-02-10 15:09:24 +0200

    Bug fix/page/home (#33)
    * adjusted the BG banner so that the text doesn't go up on a large screen resolution.
    
    * DashBoardQuery data placed on the top level
    
    * Fix text size on large screen
    
    * Add for big screens adaptive
    
    * Add new adaptive styles
    
    * Add adaptive nav bar top text and icons
    
    * Move interface from sliderbanner to SliderSettings.ts in types folder
    
    * Fix watch buttons adaptive size
    
    * Fix buttons positition on big screen
    
    * Fix border radius
    
    * Update SliderSetting interface for SliderSeasons and SliderBanner
    
    * fix footer adaptive for big screens
    
    * Format code
    
    * Add new adaptive style fix.
    
    * fix sliderseasonsarrows adaptive position, fix sliderseasons adaptive size, position
    
    * Delete console.log
    
    * Add params to ResponsiveSliderSettings.
    Refactor WatchButton component and styles.
    Refactor SectionTitle styles.
    Refactor SliderButton(BannerArrows) styles.
    
    * Add responsive settings for SliderSeasons.
    Refactor BgBanner styles, SliderBanner and SliderSeasons.
    
    * Update margins and cursor styles in SeasonArrows and SliderSeasons components.
    
    * Update SliderSeasons.scss styles.
    
    * Set infinite to false in SliderSeasons component.
    
    * Set infinitive to true for ResponsiveSliderSettings in SliderSeasons component.
    
    ---------
    
    Co-authored-by: Kostek <ksytnik73kwer@gmail.com>

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -8,6 +8,11 @@ import { useDashBoardQuery } from "../../hooks/useDashBoardQuery";
 function Home() {
     const DashBoardQuery = useDashBoardQuery();
 
+    const banners = DashBoardQuery.data?.banners;
+    const popular_seasons = DashBoardQuery.data?.popular_seasons;
+    const announced_seasons = DashBoardQuery.data?.announced_seasons;
+    const released_seasons = DashBoardQuery.data?.released_seasons;
+
     if (DashBoardQuery.isLoading) {
         <h1> Loading ....</h1>;
     }
@@ -16,18 +21,22 @@ function Home() {
         <h1> Error ....</h1>;
     }
 
-    const banners = DashBoardQuery.data?.banners;
-    const popular_seasons = DashBoardQuery.data?.popular_seasons;
-    const announced_seasons = DashBoardQuery.data?.announced_seasons;
-    const released_seasons = DashBoardQuery.data?.released_seasons;
-
     return (
         <div className="home">
             <NavigationBar />
             <BgBanner bannerData={banners || []} />
-            <SliderSeasons title="Popular Seasons" sliderData={popular_seasons || []} />
-            <SliderSeasons title="Announced Seasons" sliderData={announced_seasons || []} />
-            <SliderSeasons title="Released Seasons" sliderData={released_seasons || []} />
+            <SliderSeasons
+                title="Popular Seasons"
+                sliderData={popular_seasons || []}
+            />
+            <SliderSeasons
+                title="Announced Seasons"
+                sliderData={announced_seasons || []}
+            />
+            <SliderSeasons
+                title="Released Seasons"
+                sliderData={released_seasons || []}
+            />
             <Footer />
         </div>
     );
