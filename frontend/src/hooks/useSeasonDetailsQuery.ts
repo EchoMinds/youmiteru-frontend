6fc3be68baf4625d28ec8bfe5a8fb4fb74554841
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2024-02-18 13:03:46 +0100

    Add useSeasonDetailsQuery hook and SeasonDetails type. (#49)

---
+++ frontend/src/hooks/useSeasonDetailsQuery.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { BASE_API_URL } from "../constants/api";
+import axios from "axios";
+import { SeasonDetails } from "../types/SeasonDetails";
+
+export const useSeasonDetailsQuery = () => {
+    return useQuery({
+        queryKey: ["season-details"],
+        queryFn: async() => {
+            const result = await axios.get(`${BASE_API_URL}/season_details`);
+            return result.data as SeasonDetails;
+        }
+    });
+};
