2966d57b3e3a2af256108593b0d15d0284c3576a
Taps73r <povroznykmisha@gmail.com>
2024-02-21 19:02:12 +0200

    Bug fix

--- frontend/src/components/AuthButton/DiscordAuthButton/DiscordAuthButton.tsx
+++ frontend/src/components/AuthButton/DiscordAuthButton/DiscordAuthButton.tsx
@@ -5,7 +5,11 @@ import DiscordLogo from "./../../../assets/icons/discord-login-logo.svg";
 interface DiscordAuthButtonProps {}
 
 function DiscordAuthButton({}: DiscordAuthButtonProps): JSX.Element {
-    return <div className="discord-auth-btn">{DiscordLogo}</div>;
+    return (
+        <div className="discord-auth-btn">
+            <img src={DiscordLogo} alt="Discord Logo" />
+        </div>
+    );
 }
 
 export default DiscordAuthButton;
