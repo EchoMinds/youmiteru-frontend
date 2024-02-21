0b2cffb6fd7fafc17da6de0b66c1e6658a6621de
Taps73r <povroznykmisha@gmail.com>
2024-02-21 19:11:28 +0200

    add text and little style

--- frontend/src/components/AuthButton/DiscordAuthButton/DiscordAuthButton.tsx
+++ frontend/src/components/AuthButton/DiscordAuthButton/DiscordAuthButton.tsx
@@ -8,6 +8,7 @@ function DiscordAuthButton({}: DiscordAuthButtonProps): JSX.Element {
     return (
         <div className="discord-auth-btn">
             <img src={DiscordLogo} alt="Discord Logo" />
+            <p>Продолжить с Discord</p>
         </div>
     );
 }
