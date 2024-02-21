31634f28b04c498b8a26c697e11510d3c4b72acb
Taps73r <povroznykmisha@gmail.com>
2024-02-21 19:00:30 +0200

    Import DiscordAuthButton

--- frontend/src/components/AuthButton/AuthButtons.tsx
+++ frontend/src/components/AuthButton/AuthButtons.tsx
@@ -1,9 +1,14 @@
 import "./AuthButtons.scss";
+import DiscordAuthButton from "./DiscordAuthButton/DiscordAuthButton";
 
 interface AuthButtonProps {}
 
 function AuthButton({}: AuthButtonProps): JSX.Element {
-    return <div>AuthButton</div>;
+    return (
+        <div>
+            <DiscordAuthButton />
+        </div>
+    );
 }
 
 export default AuthButton;
