fdf90ce110c8394b31bc51825979cca0618c8117
Taps73r <povroznykmisha@gmail.com>
2024-02-21 18:56:35 +0200

    initialize DiscordAuthButton component

---
+++ frontend/src/components/AuthButton/DiscordAuthButton/DiscordAuthButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface DiscordAuthButtonProps {}
+
+function DiscordAuthButton({}: PrDiscordAuthButtonPropsops): JSX.Element {
+    return <div className="discord-auth-btn"></div>;
+}
+
+export default DiscordAuthButton;
