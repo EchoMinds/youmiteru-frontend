fb5c95ddd26070df536c97daf7c85db8728bc3cf
Taps73r <povroznykmisha@gmail.com>
2024-02-22 16:44:48 +0200

    add login page to app tsx browser router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ROUTES } from "./constants/routes";
 import Home from "./pages/Home/Home";
 import Catalog from "./pages/Catalog/Catalog";
-
+import Login from "./pages/Login/Login";
 
 function App() {
-
     return (
         <BrowserRouter>
             <Routes>
@@ -16,20 +15,27 @@ function App() {
                 />
 
                 <Route 
-                    path={ROUTES.CATALOG}
-                    element={<Catalog />}
+                    path={ROUTES.CATALOG} 
+                    element={<Catalog />} 
                 />
-                
+
                 <Route 
-                    path={ROUTES.HELP}
+                    path={ROUTES.HELP} 
                     element={<h1>Help Page</h1>} 
                 />
-                
-                <Route 
-                    path="*" 
-                    element={<h1>!Undefined Page! please come back at main page</h1>} 
-                /> 
-            </Routes>  
+
+                <Route
+                    path="*"
+                    element={
+                        <h1>!Undefined Page! please come back at main page</h1>
+                    }
+                />
+
+                <Route
+                    path={ROUTES.LOGIN} 
+                    element={<Login />} 
+                />
+            </Routes>
         </BrowserRouter>
     );
 }
