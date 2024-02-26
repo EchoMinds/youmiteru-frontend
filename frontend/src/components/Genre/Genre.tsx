80cbbcb0205b55023ad734e47d8454103f86af04
Taps73r <povroznykmisha@gmail.com>
2024-02-26 17:19:53 +0200

    add handle selected chackbox

--- frontend/src/components/Genre/Genre.tsx
+++ frontend/src/components/Genre/Genre.tsx
@@ -5,10 +5,28 @@ import { CSSTransition } from "react-transition-group";
 interface GenreProps {}
 
 function Genre({}: GenreProps): JSX.Element {
-    const [isContentVisible, setIsContentVisible] = useState(false);
+    const [isContentVisible, setIsContentVisible] = useState<boolean>(false);
+    const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
 
     const toggleContentVisibility = () => {
         setIsContentVisible(!isContentVisible);
+    };
+
+    const handleCheckboxChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const { value } = event.target;
+        setSelectedGenres((prevGenres) => {
+            if (prevGenres.includes(value)) {
+                return prevGenres.filter((genre) => genre !== value);
+            } else {
+                return [...prevGenres, value];
+            }
+        });
+    };
+
+    const handleSelectedGenresSubmit = () => {
+        console.log("Selected genres:", selectedGenres);
     };
 
     return (
@@ -21,14 +39,59 @@ function Genre({}: GenreProps): JSX.Element {
                 unmountOnExit
             >
                 <ul className="genre-list">
-                    <li>Фантастика</li>
-                    <li>Драма</li>
-                    <li>Комедия</li>
-                    <li>Кодомо</li>
-                    <li>Сёнэн</li>
-                    <li>Киберпанк</li>
+                    <li>
+                        <input
+                            type="checkbox"
+                            value="Фантастика"
+                            onChange={handleCheckboxChange}
+                        />{" "}
+                        Фантастика
+                    </li>
+                    <li>
+                        <input
+                            type="checkbox"
+                            value="Драма"
+                            onChange={handleCheckboxChange}
+                        />{" "}
+                        Драма
+                    </li>
+                    <li>
+                        <input
+                            type="checkbox"
+                            value="Комедия"
+                            onChange={handleCheckboxChange}
+                        />{" "}
+                        Комедия
+                    </li>
+                    <li>
+                        <input
+                            type="checkbox"
+                            value="Кодомо"
+                            onChange={handleCheckboxChange}
+                        />{" "}
+                        Кодомо
+                    </li>
+                    <li>
+                        <input
+                            type="checkbox"
+                            value="Сёнэн"
+                            onChange={handleCheckboxChange}
+                        />{" "}
+                        Сёнэн
+                    </li>
+                    <li>
+                        <input
+                            type="checkbox"
+                            value="Киберпанк"
+                            onChange={handleCheckboxChange}
+                        />{" "}
+                        Киберпанк
+                    </li>
                 </ul>
             </CSSTransition>
+            <button onClick={handleSelectedGenresSubmit}>
+                Подтвердить выбор
+            </button>
         </div>
     );
 }
