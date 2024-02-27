d9a6d2f2d4684021b9d77ebba20d9859a0ab8050
Taps73r <povroznykmisha@gmail.com>
2024-02-27 15:03:11 +0200

    add useState to catch selected genres and show/hide menu

--- frontend/src/components/CatalogFilter/Genre/Genre.tsx
+++ frontend/src/components/CatalogFilter/Genre/Genre.tsx
@@ -1,11 +1,96 @@
+import { useState } from "react";
 import "./Genre.scss";
 
 interface GenreProps {}
 
-function Genre({}: GenreProps) {
-    return <div className="genre">
-        <button>Жанры</button>
-    </div>;
+function Genre({}: GenreProps): JSX.Element {
+    const [genreMenu, setGenreMenu] = useState<boolean>(false);
+    const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+
+    const toggleGenreMenu = () => {
+        setGenreMenu(!genreMenu);
+    };
+
+    const handleCheckboxChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const { value, checked } = event.target;
+        if (checked) {
+            setSelectedGenres((prevSelectedGenres) => [
+                ...prevSelectedGenres,
+                value,
+            ]);
+        } else {
+            setSelectedGenres((prevSelectedGenres) =>
+                prevSelectedGenres.filter((genre) => genre !== value)
+            );
+        }
+    };
+
+    const handleApplySelection = () => {
+        console.log("Выбранные жанры:", selectedGenres);
+    };
+
+    return (
+        <div className="genre">
+            <button onClick={toggleGenreMenu}>Жанры</button>
+            {genreMenu && (
+                <div className="genre__menu">
+                    <ul>
+                        <li>
+                            <input
+                                type="checkbox"
+                                value="Фантастика"
+                                onChange={handleCheckboxChange}
+                            />{" "}
+                            Фантастика
+                        </li>
+                        <li>
+                            <input
+                                type="checkbox"
+                                value="Драма"
+                                onChange={handleCheckboxChange}
+                            />{" "}
+                            Драма
+                        </li>
+                        <li>
+                            <input
+                                type="checkbox"
+                                value="Комедия"
+                                onChange={handleCheckboxChange}
+                            />{" "}
+                            Комедия
+                        </li>
+                        <li>
+                            <input
+                                type="checkbox"
+                                value="Кодомо"
+                                onChange={handleCheckboxChange}
+                            />{" "}
+                            Кодомо
+                        </li>
+                        <li>
+                            <input
+                                type="checkbox"
+                                value="Сёнэн"
+                                onChange={handleCheckboxChange}
+                            />{" "}
+                            Сёнэн
+                        </li>
+                        <li>
+                            <input
+                                type="checkbox"
+                                value="Киберпанк"
+                                onChange={handleCheckboxChange}
+                            />{" "}
+                            Киберпанк
+                        </li>
+                    </ul>
+                    <button onClick={handleApplySelection}>Выбрать</button>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Genre;
