168e2de105b9c9ea2cd0755e4a846045cfe3e585
Taps73r <povroznykmisha@gmail.com>
2024-03-01 15:53:58 +0200

    add ul list of inputs and flex styles with width

--- frontend/src/components/CatalogFilter/AgeRating/AgeRating.tsx
+++ frontend/src/components/CatalogFilter/AgeRating/AgeRating.tsx
@@ -4,16 +4,29 @@ interface AgeRatingProps {}
 
 function AgeRating({}: AgeRatingProps): JSX.Element {
     return (
-        <div>
+        <div className="age-raiting">
+            <p>Возрастной рейтинг</p>
             <ul>
                 <li>
-                    <input type="checkbox" name="" id="" />
+                    <input
+                        type="checkbox"
+                        value="Empty"
+                    />
+                    Empty
                 </li>
                 <li>
-                    <input type="checkbox" name="" id="" />
+                    <input
+                        type="checkbox"
+                        value="16+"
+                    />
+                    16+
                 </li>
                 <li>
-                    <input type="checkbox" name="" id="" />
+                    <input
+                        type="checkbox"
+                        value="18+"
+                    />
+                    18+
                 </li>
             </ul>
         </div>
