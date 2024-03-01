04dfc06f71415eb4cc68a4480d434f1faaf789a7
Taps73r <povroznykmisha@gmail.com>
2024-03-01 16:06:59 +0200

    init fetch anime button component

---
+++ frontend/src/components/CatalogFilter/FetchAnimeButton/FetchAnimeButton.tsx
@@ -0,0 +1,13 @@
+import "./FetchAnimeButton.scss";
+
+interface FetchAnimeButtonProps {}
+
+function FetchAnimeButton({}: FetchAnimeButtonProps) {
+    return (
+        <div className="fetch-anime-button">
+            <button>Submit</button>
+        </div>
+    );
+}
+
+export default FetchAnimeButton;
