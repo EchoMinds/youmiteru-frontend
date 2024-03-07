2038a3e53e4ea64aa3069b3621465c8a19be2cf4
Taps73r <povroznykmisha@gmail.com>
2024-03-07 22:09:10 +0200

    add useCatalogQuery

---
+++ frontend/src/hooks/useCatalogQuery.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { CatalogData, CatalogParams } from "../types/Catalog";
+
+export const useCatalogQuery = (params?: CatalogParams) => {
+    return useQuery({
+        queryKey: ["catalog"],
+        queryFn: async () => {
+            const result = await axios.get(
+                "http://localhost:8080/api/title/catalog",
+                {
+                    params: params,
+                    paramsSerializer: {
+                        indexes: null,
+                    },
+                }
+            );
+            console.log(result);
+            return result.data as CatalogData;
+        },
+        enabled: false,
+    });
+};
