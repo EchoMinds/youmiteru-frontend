f808ee345922693318654e4d99b5df52b4b31501
Taps73r <povroznykmisha@gmail.com>
2024-03-07 22:11:32 +0200

    add useState, useEffect to use useCatalogQuery on page loading and button click

--- frontend/src/pages/Catalog/Catalog.tsx
+++ frontend/src/pages/Catalog/Catalog.tsx
@@ -2,13 +2,41 @@ import "./Catalog.scss";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import { Footer } from "../../components/Footer/Footer";
 import CatalogFilter from "../../components/CatalogFilter/CatalogFilter";
+import { useCatalogQuery } from "../../hooks/useCatalogQuery";
+import { CatalogParams } from "../../types/Catalog";
+import { useEffect, useState } from "react";
 
 function Catalog() {
+    const [filterParams, setFilterParams] = useState<Partial<CatalogParams>>(
+        {}
+    );
+
+    const CatalogQuery = useCatalogQuery(filterParams);
+
+    useEffect(() => {
+        void CatalogQuery.refetch();
+    }, [filterParams]);
+
+    const anime_data = CatalogQuery.data;
+
+    if (CatalogQuery.isLoading) {
+        <h1> Loading ....</h1>;
+    }
+
+    if (CatalogQuery.isError) {
+        <h1> Error ....</h1>;
+    }
+
+    const handleFilterAnime = (params: CatalogParams) => {
+        setFilterParams(params);
+        console.log(params);
+    };
+
     return (
         <div className="catalog">
             <NavigationBar />
             <div className="catalog__container">
-                <CatalogFilter />
+                <CatalogFilter handleFilterAnime={handleFilterAnime} />
             </div>
             <Footer />
         </div>
