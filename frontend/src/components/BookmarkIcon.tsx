8888f3b96b4d169071bdeafa826a743e5869e35d
Kostek <ksytnik73kwer@gmail.com>
2024-03-27 22:13:51 +0100

    Add @emotion/react, @emotion/styled, and @mui/material dependencies Add bookmark1.svg icon Add BookmarkIcon component

---
+++ frontend/src/components/BookmarkIcon.tsx
@@ -0,0 +1,10 @@
+import BookmarkLogo from "../assets/icons/bookmark1.svg";
+
+export default function BookmarkIcon() {
+    return (
+        <img
+            src={BookmarkLogo}
+            alt="bookmark-icon"
+        />
+    );
+}
