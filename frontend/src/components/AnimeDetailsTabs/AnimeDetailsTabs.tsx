59a8f0a9e2e87190870d9a3f9a9676f74cd5b478
Kostek <ksytnik73kwer@gmail.com>
2024-03-30 18:07:39 +0100

    Add related seasons to AnimeDetailsTabs component Refactor Content for Tabs

--- frontend/src/components/AnimeDetailsTabs/AnimeDetailsTabs.tsx
+++ frontend/src/components/AnimeDetailsTabs/AnimeDetailsTabs.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import ScrollableDescription from "./ScrorrableText";
+import ChronologySeason from "./Chronology";
+import { styled } from "@mui/material/styles";
+
+interface ChronologySeason {
+    seasonId: number;
+    imageUrl: string;
+}
 
 interface AnimeDetailsTabsProps {
     fullDescription: string;
+    relatedSeasons: ChronologySeason[];
 }
+
+const ContentTabs = styled(Box)(({ theme }) => ({
+    maxHeight: 500,
+    paddingLeft: 16,
+    display: "flex",
+    gap: 30,
+    maxWidth: 1200,
+    [theme.breakpoints.down("sm")]: {
+        display: "flex",
+        flexDirection: "column",
+        gap: 16,
+    },
+}));
 
 export default function AnimeDetailsTabs({
     fullDescription,
+    relatedSeasons, 
 }: AnimeDetailsTabsProps) {
     const [tabValue, setTabValue] = React.useState(0);
 
@@ -23,7 +44,7 @@ export default function AnimeDetailsTabs({
         case 0:
             return fullDescription;
         case 1:
-            return "Хронология аниме";
+            return relatedSeasons;
         default:
             return null;
         }
@@ -37,21 +58,26 @@ export default function AnimeDetailsTabs({
                 aria-label="anime details tabs"
                 sx={{ padding: 2, borderColor: "#0bffd5" }}>
                 <Tab sx={{ color: "#fff", fontWeight: 700 }} label="Описание" />
+
                 <Tab
                     sx={{ color: "#fff", fontWeight: 700 }}
                     label="Хронология"
                 />
             </Tabs>
 
-            <Box sx={{ maxHeight: 500, paddingLeft: 3 }}>
+            <ContentTabs >
                 {tabValue === 0 ? (
                     <ScrollableDescription description={tabContent(tabValue)} />
                 ) : (
-                    <Typography>
-                        {tabContent(tabValue)}
-                    </Typography>
+                    relatedSeasons.map((season) => (
+                        <ChronologySeason
+                            key={season.seasonId}
+                            seasonId={season.seasonId}
+                            imageUrl={season.imageUrl}
+                        />
+                    ))
                 )}
-            </Box>
+            </ContentTabs>
         </Box>
     );
 }
