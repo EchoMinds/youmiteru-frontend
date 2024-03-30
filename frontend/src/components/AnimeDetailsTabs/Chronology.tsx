0845b7b937acfd5182eadc663d3a6af4b98f7039
Kostek <ksytnik73kwer@gmail.com>
2024-03-30 17:02:34 +0100

    Add @mui/icons-material package and create Chronology and ScrollableDescription components

---
+++ frontend/src/components/AnimeDetailsTabs/Chronology.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+
+export default function ChronologySeasons() {
+    return (
+        <div>
+
+        </div>
+    );
+}
