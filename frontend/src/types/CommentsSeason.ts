b98aca4053b870984e8b50e66e0e438c89d40bd1
Kostek <ksytnik73kwer@gmail.com>
2024-04-06 21:08:29 +0200

    Add CommentsSection and SeasonComments components Create types for these components

---
+++ frontend/src/types/CommentsSeason.ts
@@ -0,0 +1,14 @@
+export interface LeaveCommentProps {
+    profile_avatar_url: string;
+}
+
+export interface SeasonCommentsProps {
+    profile_avatar_url: string;
+    comments: string[];
+}
+
+export interface CommentsSectionProps {
+    comments: string[];
+    profile_avatar_url: string;
+}
+
